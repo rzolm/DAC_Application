289af90c019e0daa040c5ef6bae09e7c06ee282b
rzolm <76972716+rzolm@users.noreply.github.com>
2021-05-21 12:49:00 +0100

    error in 'Add' method resolved

--- internal/forms/errors.go
+++ internal/forms/errors.go
@@ -4,14 +4,14 @@ type errors map[string][]string
 
 //adds an error message for a given field
 func (e errors) Add(field, message string) {
-    e[field] = append(e,[field], message)
+	e[field] = append(e[field], message)
 }
 
 //returns the first error message
 func (e errors) Get(field string) string {
-    es := e[field]
-    if lens(es) == 0 {
-        return ""
-    }
-    return es[0]
+	es := e[field]
+	if len(es) == 0 {
+		return ""
+	}
+	return es[0]
 }
