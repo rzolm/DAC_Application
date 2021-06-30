87c68236c03dfe47aed526aec4ee9b3e2d199780
rzolm <76972716+rzolm@users.noreply.github.com>
2021-06-30 18:25:51 +0100

    Add files via upload

--- pkg/models/template_data.go
+++ pkg/models/template_data.go
@@ -1,13 +1,17 @@
 package models
 
+import "github.com/rzolm/DAC_Application/internal/forms"
+
 //TemplateData holds data sent from handlers to templates
 type TemplateData struct {
-	StringMap    map[string]string
-	IntMap       map[string]int
-	FloatMap     map[string]float32
-	Data         map[string]interface{}
-	CSRFToken    string //crossed site request forgery token
-	Notification string
-	Warning      string
-	Error        string
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string //crossed site request forgery token
+	Notification    string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
